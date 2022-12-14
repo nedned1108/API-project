38540164deae78e20ce4b464a7570ce5277fefc6
nednguyen <rpmshockwave@gmail.com>
2022-12-14 16:46:49 -0500

    Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -9,8 +9,10 @@ const cookieParser = require('cookie-parser');
 // checking if environment is in production or not
 const { environment } = require('./config');
 const isProduction = environment === 'production';
-// initialize express application
-const app = express();
+const app = express(); // initialize express application
+const routes = require('./routes'); // import routes
+const { ValidationError } = require('sequelize');
+
 // Connect morgan middleware
 app.use(morgan('dev'));
 // parsing cookies and parsing JSON bodies of requests
@@ -41,11 +43,35 @@ app.use(
     })
 );
 
-
-const routes = require('./routes');
-
 app.use(routes); // Connect all the routes
 
-
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+});
+// Error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
 
 module.exports = app;
