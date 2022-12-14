f254dcea6f8684503e457acb548d12feccb0d116
nednguyen <rpmshockwave@gmail.com>
2022-12-14 15:59:22 -0500

    phase 0 first commit

---
+++ backend/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+// checking if environment is in production or not
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+// initialize express application
+const app = express();
+// Connect morgan middleware
+app.use(morgan('dev'));
+// parsing cookies and parsing JSON bodies of requests
+app.use(cookieParser());
+app.use(express.json());
+
+// Security Middleware
+if (!isProduction) {
+    // enable cors only in development
+    app.use(cors());
+}
+
+// helmet helps set a variety of headers to better secure your app
+app.use(
+    helmet.crossOriginResourcePolicy({
+        policy: "cross-origin"
+    })
+);
+
+// Set the _csrf token and create req.csrfToken method
+app.use(
+    csurf({
+        cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+        }
+    })
+);
+
+
+const routes = require('./routes');
+
+app.use(routes); // Connect all the routes
+
+
+
+module.exports = app;
