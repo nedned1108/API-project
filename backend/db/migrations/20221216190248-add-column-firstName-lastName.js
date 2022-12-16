8d8b0293d145ba0ce04287565d6bd7984ed06df6
nednguyen <rpmshockwave@gmail.com>
2022-12-16 16:43:04 -0500

    Add user firstName and lastName attributes

---
+++ backend/db/migrations/20221216190248-add-column-firstName-lastName.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+module.exports = {
+   up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('Users', 'firstName', {
+      type: Sequelize.STRING,
+    }, options);
+
+    await queryInterface.addColumn('Users', 'lastName', {
+      type: Sequelize.STRING,
+    }, options);
+  },
+
+   down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    options.tableName = "Users";
+    await queryInterface.removeColumn(options, 'firstName');
+    await queryInterface.removeColumn(options, 'lastName');
+  }
+};
