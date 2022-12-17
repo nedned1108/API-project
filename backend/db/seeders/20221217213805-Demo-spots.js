de490552af5ceb8f4a6c89c344f1328c11e85d22
nednguyen <rpmshockwave@gmail.com>
2022-12-17 16:55:58 -0500

    Create spots model, migration and demo spots

---
+++ backend/db/seeders/20221217213805-Demo-spots.js
@@ -0,0 +1,63 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    options.tableName = 'Spots';
+    return queryInterface.bulkInsert(options, [
+      {
+        ownerId: 1,
+        address: "123 Disney Lane",
+        city: "San Francisco",
+        state: "California",
+        country: "United States of America",
+        lat: 37.7645358,
+        lng: -122.4730327,
+        name: "App Academy",
+        description: "Place where web developers are created",
+        price: 123,
+      },
+      {
+        ownerId: 2,
+        address: "4883 Camaron Drive",
+        city: "Atlanta",
+        state: "Georgia",
+        country: "United States of America",
+        lat: 159.24356728,
+        lng: -122.47303274,
+        name: "Eddie",
+        description: "Home sweet home",
+        price: 553,
+      }
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    options.tableName = 'Spots';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      ownerId: { [Op.in]: [1, 2] }
+    }, {});
+  }
+};
