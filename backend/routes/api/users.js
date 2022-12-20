cc81148a4f8e6357cb39ac294aa8f85854e2f351
nednguyen <rpmshockwave@gmail.com>
2022-12-20 00:06:59 -0500

    update sign up user

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -42,13 +42,54 @@ router.post(
     validateSignup,
     async (req, res) => {
         const { email, password, username, firstName, lastName } = req.body;
-        const user = await User.signup({ email, username, password, firstName, lastName });
-    
-        await setTokenCookie(res, user);
-    
-        return res.json({
-            user: user
+        // Error response: User already exists with the specified email
+        const existingEmail = await User.findOne({
+            where: {
+                email: email
+            }
         });
+        const existingUsername = await User.findOne({
+            where: {
+                username: username
+            }
+        })
+        if (existingEmail) {
+            res.status(403),
+            res.json(
+            {
+                "message": "User already exists",
+                "statusCode": 403,
+                "errors": {
+                    "email": "User with that email already exists"
+                }
+            })
+        } else if (existingUsername) {            // Error response: User already exists with the specified username
+            res.status(403),
+            res.json(
+            {
+                "message": "User already exists",
+                "statusCode": 403,
+                "errors": {
+                    "email": "User with that username already exists"
+                }
+            })
+        } else if (!existingEmail && ! existingUsername) {
+            let user = await User.signup({ email, username, password, firstName, lastName }, {scope: 'currentUser'});
+            let token = await setTokenCookie(res, user);
+            user = user.toJSON();
+            user.token = token;
+            return res.json(user);
+        } else if (!email) {
+            res.json(
+                {
+                    "message": "Validation error",
+                    "statusCode": 400,
+                    "errors": {
+                      "email": "Invalid email",
+                    }
+                }
+            )
+        }
     }
 );
 
