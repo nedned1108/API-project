053d6f435c68be0e2cfbad59ab97dabad3f7e7bd
nednguyen <rpmshockwave@gmail.com>
2023-01-13 14:39:56 -0500

    update LoginFormPage

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -39,25 +39,26 @@ function LoginFormPage() {
   return (
     <form onSubmit={handleSubmit}>
       <ul>
-        <label>
-          Username or Email
-          <input
-            type='text'
-            value={credential}
-            onChange={(e) => setCredential(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type='password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
+        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
       </ul>
+      <label>
+        Username or Email
+        <input
+          type='text'
+          value={credential}
+          onChange={(e) => setCredential(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Password
+        <input
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
       <button type='submit' >Log In</button>
     </form>
   )
