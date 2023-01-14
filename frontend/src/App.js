bd134bf9dbe53e9cdc70a6b689e58c737b84d919
nednguyen <rpmshockwave@gmail.com>
2023-01-14 18:14:32 -0500

    FE phase 4 Login form Modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,12 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Switch } from "react-router-dom";
-import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from './components/SignupFormPage';
-import Navigation from './components/Navigation';
 import * as sessionActions from './store/session';
+import Navigation from './components/Navigation';
 
 
 function App() {
-
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
@@ -21,9 +19,6 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
-          <Route path='/login'>
-            <LoginFormPage />
-          </Route>
           <Route path='/signup'>
             <SignupFormPage />
           </Route>
