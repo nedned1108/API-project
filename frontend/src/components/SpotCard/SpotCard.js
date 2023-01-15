5f874609ac9f3161e2ca68eba92dee0bfbd6015b
nednguyen <rpmshockwave@gmail.com>
2023-01-15 18:02:00 -0500

    get all Spots css

--- frontend/src/components/SpotCard/SpotCard.js
+++ frontend/src/components/SpotCard/SpotCard.js
@@ -2,14 +2,21 @@ import { useDispatch } from "react-redux";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import './SpotCard.css'
+import comingSoon from '../../Image/Image_Coming_Soon.png'
 
 const SpotCard = ({ spot }) => {
   const dispatch = useDispatch();
-  
+
   return (
     <div className="spotCard-main-div">
-      <img className="spot-previewImage" src={spot.previewImage} />
-      <Link className="spot-city-state" to={`/${spot.id}`}>{spot.city}, {spot.state}</Link>
+      <Link className="spot-city-state" to={`/${spot.id}`}>
+        <img className="spot-previewImage" src={(spot.previewImage) ? spot.previewImage : comingSoon} />
+        <p>{spot.city}, {spot.state}</p>
+        <div className="price-div">
+          <p>${spot.price}</p>
+          <p className="price-night" >night</p>
+        </div>
+      </Link>
     </div>
   )
 };
