4422bf9f38a3bbddad7d7a7a3d7ce150235e09b3
nednguyen <rpmshockwave@gmail.com>
2023-01-25 23:51:58 -0500

    create booking reducer

---
+++ frontend/src/store/bookings.js
@@ -0,0 +1,8 @@
+//frontend/src/store/bookings.js
+import { csrfFetch } from "./csrf";
+
+const LOAD_ALLBOOKINGS = 'allSpots/LOAD_ALLBOOKINGS';
+const LOAD_BOOKING = 'singleSpot/LOAD_BOOKING';
+const CREATE_BOOKING = 'singleSpot/CREATE_BOOKING';
+const UPDATE_BOOKING = 'singleSpot/UPDATE_BOOKING';
+const DELETE_BOOKING = 'singleSpot/DELETE_BOOKING';
