f2a929f98d8c06adee464ebfb9a7ddb37a908e9f
nednguyen <rpmshockwave@gmail.com>
2023-05-17 15:23:08 -0400

    add search logic

--- frontend/src/components/Navigation/SearchBar.js
+++ frontend/src/components/Navigation/SearchBar.js
@@ -1,20 +1,43 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { thunkLoadAllSpots } from "../../store/spots";
 
 function SearchBar () {
   const dispatch = useDispatch();
+  const allSpotsData = useSelector(state => state.spots.allSpots);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    dispatch(thunkLoadAllSpots())
+  }, [dispatch])
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchTerm(e.target.value);
+
+    const results = performSearch(searchTerm);
+    setSearchResults(results);
+  };
+
+  const performSearch = (e) => {
+    e.preventDefault();
+    dispatch(searchTerm);
+  };
 
   return (
     <div className="search-bar">
-      <form className="search-bar-form">
         <input
           type="text"
           placeholder="Search"
+          className="search-bar-input"
+          onChange={handleSearch}
         />
-        <button type="submit">
-          <i className="fas fa-search"></i>
-        </button>
-      </form>
+        <ul>
+          {searchResults.map((result) => (
+            <li key={result.id}>{result.name}</li>
+          ))}
+        </ul>
     </div>
   )
 }
